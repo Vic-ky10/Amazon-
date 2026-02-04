369c1f9e41b4dcc8e72a7245f07e7fca7bc157ba
vigneshwaran <vigneshvenketasan@gmail.com>
2026-02-04 17:26:10 +0530

    created and update the checkout.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,29 @@
-export  const cart = [ ] ;
+﻿export const cart = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
+
+export function addToCart(productId) {
+  let matchingcartItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingcartItem = cartItem;
+    }
+  });
+
+  if (matchingcartItem) {
+    matchingcartItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
+
