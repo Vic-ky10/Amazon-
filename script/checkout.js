36d65ae5e9f0ff4990554fcad055d674849b138d
vigneshwaran <vigneshvenketasan@gmail.com>
2026-02-06 16:01:10 +0530

    Updated

---
+++ script/checkout.js
@@ -0,0 +1,8 @@
+﻿
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+
+renderOrderSummary();
+
+renderPaymentSummary()
---
+++ script/checkout/paymentSummary.js
@@ -0,0 +1,5 @@
+import { cart } from "../../data/cart.js";
+
+export function renderPaymentSummary() {
+  // TODO: implement payment summary rendering.
+}
